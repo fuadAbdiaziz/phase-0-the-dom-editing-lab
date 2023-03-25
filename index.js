bf7f9994955275331d5ef19bd4248b474ebdee64
Fuad <fuadabdaz99@gmail.com>
2023-03-25 10:50:05 -0700

    completed lab

---
+++ index.js
@@ -0,0 +1,11 @@
+// Get a reference to the body element
+const body = document.body;
+
+// Create an h1 element
+const h1 = document.createElement('h1');
+
+// Set the text content of the h1 element
+h1.textContent = 'Hello, world!';
+
+// Append the h1 element to the body
+body.appendChild(h1);
